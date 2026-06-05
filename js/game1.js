2d0d649170069afeac7b40acfdad64ebb5df8764
Evan <evan@cybership.net>
2026-06-05 10:28:53 -0500

    Adding playable games

---
+++ js/game1.js
@@ -0,0 +1,33 @@
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+let player = {
+    x: 100,
+    y: 100,
+    size: 40,
+    speed: 4
+};
+
+let keys = {};
+
+document.addEventListener("keydown", e => keys[e.key] = true);
+document.addEventListener("keyup", e => keys[e.key] = false);
+
+function update() {
+    if (keys["w"]) player.y -= player.speed;
+    if (keys["s"]) player.y += player.speed;
+    if (keys["a"]) player.x -= player.speed;
+    if (keys["d"]) player.x += player.speed;
+
+    draw();
+    requestAnimationFrame(update);
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = "#64ffda";
+    ctx.fillRect(player.x, player.y, player.size, player.size);
+}
+
+update();
