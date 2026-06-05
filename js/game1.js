21dfa327c99a2521d1b7a3ead1dcf131e48e1565
Evan <evan@cybership.net>
2026-06-05 10:34:06 -0500

    Rough Game working

--- js/game1.js
+++ js/game1.js
@@ -1,33 +1,34 @@
-const canvas = document.getElementById("game");
-const ctx = canvas.getContext("2d");
+function initGame() {
+    const canvas = document.getElementById("game");
+    const ctx = canvas.getContext("2d");
 
-let player = {
-    x: 100,
-    y: 100,
-    size: 40,
-    speed: 4
-};
+    let player = {
+        x: 100,
+        y: 100,
+        size: 40,
+        speed: 4
+    };
 
-let keys = {};
+    let keys = {};
 
-document.addEventListener("keydown", e => keys[e.key] = true);
-document.addEventListener("keyup", e => keys[e.key] = false);
+    document.addEventListener("keydown", e => keys[e.key] = true);
+    document.addEventListener("keyup", e => keys[e.key] = false);
 
-function update() {
-    if (keys["w"]) player.y -= player.speed;
-    if (keys["s"]) player.y += player.speed;
-    if (keys["a"]) player.x -= player.speed;
-    if (keys["d"]) player.x += player.speed;
+    function update() {
+        if (keys["w"]) player.y -= player.speed;
+        if (keys["s"]) player.y += player.speed;
+        if (keys["a"]) player.x -= player.speed;
+        if (keys["d"]) player.x += player.speed;
 
-    draw();
-    requestAnimationFrame(update);
+        draw();
+        requestAnimationFrame(update);
+    }
+
+    function draw() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = "#64ffda";
+        ctx.fillRect(player.x, player.y, player.size, player.size);
+    }
+
+    update();
 }
-
-function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    ctx.fillStyle = "#64ffda";
-    ctx.fillRect(player.x, player.y, player.size, player.size);
-}
-
-update();
