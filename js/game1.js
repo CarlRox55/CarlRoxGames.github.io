67f25d82693fde02b86e8578e124b4c01ef78faa
Evan <evan@cybership.net>
2026-06-05 10:58:08 -0500

    game update

--- js/game1.js
+++ js/game1.js
@@ -5,20 +5,25 @@ function initGame() {
     let player = {
         x: 100,
         y: 100,
-        size: 40,
-        speed: 12
+        size: 40
     };
+    
+    let playerSpeed = 12;
 
+    
     let playerYVel = 0;
-    let gravity = 1;
+    let grounded = false;
+    let jumpHeight = 120;
+    let gravity = 0.67;
     let groundHeight = 60;
     let XOffset = 0;
-
     let keys = {};
 
     document.addEventListener("keydown", e => keys[e.key] = true);
     document.addEventListener("keyup", e => keys[e.key] = false);
     
+
+
     update();
 
     
@@ -36,14 +41,8 @@ function initGame() {
 
     function playerMovement(){
 
-        if (keys["a"]) player.x -= player.speed;
-        if (keys["d"]) player.x += player.speed;
-        playerYVel += gravity;
-        player.y += playerYVel;
-        if (player.y > canvas.height - groundHeight - player.size){
-            player.y = canvas.height - groundHeight - player.size;
-            playerYVel = 0;
-        }
+        if (keys["a"]) player.x -= playerSpeed;
+        
 
         if (player.x - XOffset > canvas.width - 150) {
             XOffset = player.x - (canvas.width - 150);
@@ -51,7 +50,20 @@ function initGame() {
         if (player.x - XOffset < 150) {
             XOffset = player.x - 150;
         }
+
+        if (keys["d"]) player.x += playerSpeed;
+
+        playerYVel += gravity;
+        player.y += playerYVel;
+        if (player.y > canvas.height - groundHeight - player.size){
+            player.y = canvas.height - groundHeight - player.size;
+            playerYVel = 0;
+        }
+        grounded = player.y >= canvas.height - groundHeight - player.size;
         
+        if (keys[" "] && grounded){
+            playerYVel = -Math.sqrt(2 * gravity * jumpHeight);
+        }
     }
 
     function draw() {
